4cbfa2051bf34f4513af36895290a54a5fde7e66
miguelbrieva <miguel.brieva@gmail.com>
2020-07-20 17:34:30 -0400

    Add individual posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,6 +14,8 @@ exports.createPages = async function ({ actions, graphql }) {
     }
   `)
 
+  console.log(data)
+
   // Create paginated pages for posts
   const postPerPage = 3
   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
@@ -31,14 +33,14 @@ exports.createPages = async function ({ actions, graphql }) {
     })
   })
 
-  //   // Create single blog posts
-  //   data.allMdx.edges.forEach(edge => {
-  //     const slug = edge.node.frontmatter.slug
-  //     const id = edge.node.id
-  //     actions.createPages({
-  //       path: slug,
-  //       component: require.resolve(`./src/templates/singlePost`),
-  //       context: { id },
-  //     })
-  //   })
+  // Create single blog posts
+  data.allMdx.edges.forEach(edge => {
+    const slug = edge.node.frontmatter.slug
+    const id = edge.node.id
+    actions.createPage({
+      path: slug,
+      component: require.resolve(`./src/templates/singlePost`),
+      context: { id },
+    })
+  })
 }
